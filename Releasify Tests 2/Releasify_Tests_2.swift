97f2d90cd11836e2fed1a43057e8da7692311198
Maurice Achtenhagen <maurice@achtenhagen.me>
2015-06-12 12:10:26 -0700

    test

---
+++ Releasify Tests 2/Releasify_Tests_2.swift
@@ -0,0 +1,36 @@
+//
+//  Releasify_Tests_2.swift
+//  Releasify Tests 2
+//
+//  Created by Maurice Achtenhagen on 6/12/15.
+//  Copyright (c) 2015 Fioware Studios, LLC. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Releasify_Tests_2: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
